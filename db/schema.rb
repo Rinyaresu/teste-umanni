0e4569c3affeb555a83097144965dd5aac973b9e
rinyaresu <kaiquelinhares17@outlook.com>
2022-11-05 20:48:34 -0300

    fix: typo

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2022_11_05_231431) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_11_05_215012) do
->>>>>>> 6017c66 (feat: users)
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,11 +56,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_05_215012) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
-=======
->>>>>>> 6017c66 (feat: users)
   end
 
   create_table "users", force: :cascade do |t|
@@ -82,8 +75,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_05_215012) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-<<<<<<< HEAD
   add_foreign_key "posts", "users"
-=======
->>>>>>> 6017c66 (feat: users)
 end
