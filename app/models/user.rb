7ec1865d882654c133e5c703a8cd3b87b71b01d4
Kaique Linhares <79602114+Rinyaresu@users.noreply.github.com>
2022-11-10 13:12:08 -0300

    fix: remove admin role (#9)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: { user: 0, admin: 1 }
-  after_initialize :set_default_role, if: :new_record?
-
-  def set_default_role
-    self.role ||= :user
-  end
-
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   has_many :posts, dependent: :destroy
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  describe 'after_initialize' do
-    it 'sets default role to user' do
-      user = described_class.new
-      expect(user.role).to eq('user')
-    end
-  end
-
   describe 'associations' do
     it { is_expected.to have_many(:posts) }
     it { is_expected.to have_many(:comments) }
